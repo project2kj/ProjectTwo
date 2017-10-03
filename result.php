02da6d2408141907b6a3d7c0554db62e6792c7fa
xyzJo <joana97.ji@gmail.com>
2017-10-03 09:34:24 -0400

    Update result.php

--- result.php
+++ result.php
@@ -2,48 +2,36 @@
     <head>
         <meta http-equiv="Content-Type"
               content="text/html; charset=UTF-8">
-		<link rel="stylesheet" type="text/css" href="quizstyle.css" />
-   <link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
-   <link href='https://fonts.googleapis.com/css?family=Glass Antiqua' rel='stylesheet'>
         <title>Your Results</title>
     </head>
-   
-	<h1>Most Awesome personality test ever</h1>
-        <body>
-<p id = "talk">		<?php
-
+    <body>
+    <h1>Most Awesome personality test ever</h1>
+        <?php
         $color = $_POST['color'];
         $family = $_POST['family'];
-        $movie = $_POST['movie']; 
+        $movie = $_POST['movie'];
         $door = $_POST['door'];
         $flowers = $_POST['flowers'];
-
-
         //calculation of score
         $score = $color + $family + $movie + $door + $flowers;
-   
         echo 'Your scored '."$score".'points'; 
-	echo "<br>";
-
-        //Add if statements here using variable $score to determine personality 
-
+    echo "<br>";
             /* total of 0-25 = great personality!
              * total 25-50 = fair personality
              * total 50 and up = Horrible personality  */
         
-        if($score<=25){
-		echo "<br>";
-		echo " You have a GREAT personality. Keep it up! ‭💪";
-	}elseif($score >= 26 && $score <= 50){
-		echo "<br>";
-		echo "You have a fair personality.";			
-	}elseif($score >= 51 && $score <= 100){
-		echo "<br>";
-		echo "Have you thought about being a hermit?... HORRIBLE PERSONALITY!";
-	}
-
+        if ($score<=25) {
+          echo "<br>";
+          echo "You have a GREAT personality. Keep it up! ‭💪";
+        } elseif ($score >= 26 && $score <= 50) {
+          echo "<br>";
+          echo "You have a fair personality. 👌";            
+        } else {
+          echo "<br>";
+          echo "Have you thought about being a hermit?... HORRIBLE PERSONALITY! 💩";
+        }
         ?>
-</p>
+
 
     </body>
 </html>
